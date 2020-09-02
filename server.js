afcd09d2254bf355b9caf65d2a3f198bb7009e25
Carl Shen <carl.shen1@uwaterloo.ca>
2020-09-02 16:59:30 -0400

    Prepare for deployment v1.0

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport")
+const path = require('path')
 
 const users = require("./routes/api/users")
 const recipe = require("./routes/api/recipe")
@@ -33,6 +34,15 @@ app.use(passport.initialize());
 // Routes
 app.use("/api/users", users);
 app.use("/api/recipe", recipe);
+
+//Serve static assets if in production
+if(process.env.NODE_ENV === "production") {
+  //Set static folder
+  app.use(express.static('client/build'))
+  app.get('*', (req,res)=>{
+    res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+  })
+}
 
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
